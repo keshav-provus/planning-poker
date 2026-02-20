aebc4530b74e40a2b8b99996ab46a5b200ad8226
keshav-provus <keshav@provus.ai>
2026-02-20 14:47:32 +0530

    Created Test API

---
+++ app/api/route.ts
@@ -0,0 +1,7 @@
+import GenSession from "../../lib/genSession";
+
+export async function GET(request: Request) {
+    const { ticket } = await request.json();
+    const session = await GenSession();
+    return new Response(JSON.stringify({ session }));
+}
