e644dfe186eda2721e42f46b0c0710cc8926b618
keshav-provus <keshav@provus.ai>
2026-02-20 15:29:11 +0530

    Test with Random

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,9 +1,7 @@
 import GenSession from "../../lib/genSession";
 
 export async function GET(request: Request) {
-    // const { ticket } = await request.json();
-    // const session = await GenSession();
-    // return new Response(JSON.stringify({ session }));
-
-    return new Response(JSON.stringify({ session: "123" }));
+    const { ticket } = await request.json();
+    const session = await GenSession();
+    return new Response(JSON.stringify({ session }));
 }
