1cc4eb1270747bcc90f97b3e290f08386feefe7d
keshav-provus <keshav@provus.ai>
2026-02-20 15:14:42 +0530

    TESTING ROUTE

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,7 +1,9 @@
 import GenSession from "../../lib/genSession";
 
 export async function GET(request: Request) {
-    const { ticket } = await request.json();
-    const session = await GenSession();
-    return new Response(JSON.stringify({ session }));
+    // const { ticket } = await request.json();
+    // const session = await GenSession();
+    // return new Response(JSON.stringify({ session }));
+
+    return new Response(JSON.stringify({ session: "123" }));
 }
